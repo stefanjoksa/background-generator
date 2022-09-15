e05bf6aa00da1e4fb7e667b29a044695749ecad7
stefanjoksa <stefanjoxa@gmail.com>
2022-09-15 15:05:21 +0200

    uploading final version

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-
+var button = document.querySelector(".random");
 
 function setGradient() {
-	body.style.background = "linear-gradient(to right, "
+	body.style.background = 
+	"linear-gradient(to right, " 
 	+ color1.value 
 	+ ", " 
 	+ color2.value 
@@ -14,6 +15,30 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function randomHex() {
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+
+function setBackgroundOnLoad (){
+	color1.value = randomHex();
+	color2.value = randomHex();
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+setBackgroundOnLoad();
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", function(){
+	setBackgroundOnLoad();
+});
